b1375d8889e2bc21845f44f16ffd45794f71e196
alpaylan <alpkeles99@gmail.com>
2026-05-23 23:20:00 -0400

    simplify

--- loops/FuzzLoop.v
+++ loops/FuzzLoop.v
@@ -12,19 +12,17 @@ Local Open Scope nat_scope.
 Local Open Scope prop_scope.
 
 Definition fuzzLoop
-(fuel : nat) 
-(cprop : CProp ∅)
-{Pool : Type}
-{poolType: @SeedPool (⟦⦗cprop⦘⟧) Z Pool}
-(seeds : Pool)
-(utility: Utility) : G Result :=
-let fix fuzzLoop' 
+	(fuel : nat) 
+	(cprop : CProp ∅)
+	{Pool : Type}
+	{poolType: @SeedPool (⟦⦗cprop⦘⟧) Z Pool}
+	(seeds : Pool)
+	(utility: Utility) : G Result :=
+	let fix fuzzLoop' 
         (fuel : nat) 
         (passed : nat)
         (discards: nat)
-        {Pool : Type}
         (seeds : Pool)
-        (poolType: @SeedPool (⟦⦗cprop⦘⟧) Z Pool)
         (utility: Utility) : G Result :=
       match fuel with
       | O => ret (mkResult discards false passed [])
@@ -37,7 +35,7 @@ let fix fuzzLoop'
 															| Generate => seeds
 															| Mutate _ => revise seeds
 															end in
-						fuzzLoop' fuel' passed (discards + 1) seeds' poolType utility
+						fuzzLoop' fuel' passed (discards + 1) seeds' utility
 					| Some input =>
 						let '(result, feedback) := instrumented_runner cprop with_instrumentation input in
 						match result with
@@ -51,28 +49,29 @@ let fix fuzzLoop'
 								match useful seeds feedback with
 								| true =>
 										let seeds' := invest (input, feedback) seeds in
-										fuzzLoop' fuel' (passed + 1) discards seeds' poolType utility
+										fuzzLoop' fuel' (passed + 1) discards seeds' utility
 								| false =>
 										let seeds' := match directive with
 																	| Generate => seeds
 																	| Mutate _ => revise seeds
 																	end in
-										fuzzLoop' fuel' (passed + 1) discards seeds' poolType utility
+										fuzzLoop' fuel' (passed + 1) discards seeds' utility
 								end
 						| None => 
 								(* Discard *)
 								let feedback := Z.div feedback 3 in
 								match useful seeds feedback with
 								| true =>
-									fuzzLoop' fuel' passed (discards + 1) seeds poolType utility
+									(* TODO: We currently don't leverage discarded useful inputs *)
+									fuzzLoop' fuel' passed (discards + 1) seeds utility
 								| false =>
 									let seeds' := match directive with
 																| Generate => seeds
 																| Mutate _ => revise seeds
 																end in
-									fuzzLoop' fuel' passed (discards + 1) seeds' poolType utility
+									fuzzLoop' fuel' passed (discards + 1) seeds' utility
 								end
 							end
 						end
 					end in
-      fuzzLoop' fuel 0 0 seeds poolType utility.
+      fuzzLoop' fuel 0 0 seeds utility.
